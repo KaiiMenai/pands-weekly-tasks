7bb8028522743489ad9e27ca93f7d298470d3324
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 21:58:34 +0000

    updated bank.py with output in comments and references for reasoning

--- bank.py
+++ bank.py
@@ -11,6 +11,11 @@
 #Enter amount2(in cent): 180
 #The sum of these is €2.45
 
+# Process:
+# Greeting and ask for input in cents from person inputting.
+# Using input from extra reading as a guide: https://github.com/KaiiMenai/pands-mywork/blob/12940891d05aaf77403654a383bcd57e7e86c184/week02/money.py
+# Guidance from the web was used for the convert to cent formula formatting.
+
 print("Hello," + "\tPlease enter the following amounts in cents.")
 x = input("Enter amount 1: ")
 y = input("Enter amount 2: ")
@@ -20,3 +25,14 @@ def convertToCent(sum):
 
 txt = f"The sum of these is {convertToCent(sum)}."
 print(txt)
+
+# When an input is given of the values listed in the brief the outcome is as follows:
+# Hello,  Please enter the following amounts in cents.
+# Enter amount 1: 65
+# Enter amount 2: 180
+# The sum of these is €2.45.
+
+# References:
+# My own Ppands-my work repository https://github.com/KaiiMenai/pands-mywork/blob/12940891d05aaf77403654a383bcd57e7e86c184/week02/money.py
+# Answer by "Elegent" on stackoverflow (22/11/2015) https://stackoverflow.com/questions/33861401/convert-cents-to-euro
+# Further reading worksheet https://www.w3schools.com/python/python_string_formatting.asp
