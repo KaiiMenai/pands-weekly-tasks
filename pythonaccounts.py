19e1b0843b6dd998bca4379c41d9d784e5af7c3d
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-18 20:34:18 +0000

    task 3 began

---
+++ pythonaccounts.py
@@ -0,0 +1,5 @@
+# pandsaccount.py
+# This program will read in a 10 character account number and will output the account numberwith only the last 4 digits shown.
+# author: Kyra Menai Hamilton
+
+# Weekly Task 03 brief
