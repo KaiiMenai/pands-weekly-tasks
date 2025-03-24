b448259f274d40fa22b756e4ce6c8513234262cc
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-03-24 18:01:08 +0000

    wk 7 task started

---
+++ numberofes.py
@@ -0,0 +1,12 @@
+# numberofes.py
+# This program will read in a text file and print out the number of e's contained therin.
+# author: Kyra Menai Hamilton
+
+# Import modules needed for the functionality of the program.
+import sys
+import pandas as pd
+
+# Open the file in read mode.
+moby = pd.read_csv("https://gist.github.com/StevenClontz/4445774")
+
+print(moby)
