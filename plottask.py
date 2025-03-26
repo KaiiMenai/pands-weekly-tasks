08470e17beb82665c0ac99f2bc992341cd8924f8
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-03-26 21:16:31 +0000

    clarifying file structure

--- plottask.py
+++ plottask.py
@@ -2,7 +2,7 @@
 # Week 8 task
 # Write a program called plottask.py that on the one set of axes displays: (Some marks will be given for making the plot look nice (legend etc).Please put a copy of the image of the plot (.png file) into the repository)
     # a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
-    # and a plot of the function  h(x)=x3 in the range 0 to 10, 
+    # and a plot of the function h(x)=x3 in the range 0 to 10, 
 # author: Kyra Menai Hamilton
 
 import numpy as np
@@ -29,7 +29,9 @@ plt.title('Histogram of a Normal Distribution of 1000 random values \n(Mean = 5,
 plt.legend()
 plt.show()
 
-# Plot output showin in: week8task_figure_1_normaldis1000.png for histogram of normal distribution of 1000 values.
+# Plot output shown in: week8task_figure_1_normaldis1000.png for histogram of normal distribution of 1000 values.
+
+# PART 2
 
 # Now to plot the function h(x)=x^3 in the range 0 to 10.
 # Create the range of values from 0 to 10.
@@ -37,12 +39,15 @@ x = np.linspace(0, 10, 100)
 y = x**3
 
 # Plot the function h(x)=x^3 in the range 0 to 10.
+# This will make a line plot of the function h(x)=x^3 in the range 0 to 10.
 plt.plot(x, y, color='m', label='h(x)=x^3')
 plt.xlabel('x')
 plt.ylabel('h(x)')
 plt.title('Plot of the function h(x)=x^3 in the range 0 to 10.')
 plt.legend()
 plt.show()
+
+# Plot output shown in: week8task_figure_2_hx3.png for plot of the function h(x)=x^3 in the range 0 to 10.
 
 # References:
 # https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
