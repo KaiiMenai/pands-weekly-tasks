8deb77bd110bec45c40ce9e4c04e264c42f591a9
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-27 20:57:27 +0100

    title of files added and / or modified

---
+++ numberofes_txt.py
@@ -0,0 +1,5 @@
+# numberofes_txt.py
+# Week 7 task
+# This program will read in a text file and print out the number of e's contained therin.
+# author: Kyra Menai Hamilton
+
--- numberofes_url.py
+++ numberofes_url.py
@@ -1,4 +1,4 @@
-# numberofes.py
+# numberofes_url.py
 # Week 7 task
 # This program will read in a text file and print out the number of e's contained therin.
 # author: Kyra Menai Hamilton
