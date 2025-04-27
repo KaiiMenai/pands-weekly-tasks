dc014ff9869ded370baf8a0d467dfec26a1dc2f1
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-04-27 21:00:03 +0100

    adding in saving file from url

--- numberofes_txt.py
+++ numberofes_txt.py
@@ -3,3 +3,12 @@
 # This program will read in a text file and print out the number of e's contained therin.
 # author: Kyra Menai Hamilton
 
+# Import urllib.request - same as in the es from url. But now will save the url to a text file (locally - i.e. into the repository).
+import urllib.request
+from urllib.request import urlretrieve
+
+url = "https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt"
+filename = "mobydick.txt"
+
+urlretrieve(url, filename)
+print("File downloaded and saved as mobydick.txt in pands-weekly-tasks directory.")
